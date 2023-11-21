f1590ed011fb757c32d487a29a833eb4691c1921
LQ <yihao.qin17311-biph@basischina.com>
2023-11-21 11:35:27 +0800

    spring boot structure

---
+++ fianalyze/src/main/java/com/finanalyze/fianalyze/FianalyzeApplication.java
@@ -0,0 +1,13 @@
+package com.finanalyze.fianalyze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FianalyzeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FianalyzeApplication.class, args);
+	}
+
+}
