7249ceca4be8f0be05b3e7be3b17129f8c1f3f3c
LQ <143152892+LQ458@users.noreply.github.com>
2024-02-20 15:56:27 +0800

    Setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
-import { Routes } from '@angular/router';
 import { IntroComponent } from './intro/intro.component';
+import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: 'intro', component: IntroComponent },
